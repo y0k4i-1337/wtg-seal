d9ebe90e4d68a9aa7e04f368b54813947357b480
M. Choji <mchoji@users.noreply.github.com>
2020-02-26 15:31:50 -0300

    Add parser functions to utils.py and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from wtgseal import utils
 
 
@@ -8,6 +10,20 @@ def test_parse_objects(datadir):
         [4, 5, 6, 7, 8, 9, 10]
     ]
     with (datadir / 'objout.txt').open() as f:
-        parsed = utils.parse_objects(f)
-    assert parsed is not None
-    assert expected == parsed
+        it = utils.parse_objects(f)
+        for i, obj in enumerate(it):
+            assert obj == expected[i]
+
+
+def test_parse_requests(datadir):
+    expected = [1, 5, 3, 1, 1]
+    with (datadir / 'name.txt').open() as f:
+        it = utils.parse_requests(f)
+        assert expected == list(it)
+
+
+def test_count_requests(datadir):
+    expected = Counter({1: 3, 3: 1, 5: 1})
+    with (datadir / 'name.txt').open() as f:
+        frequencies = utils.count_requests(f)
+        assert sorted(expected.most_common()) == sorted(frequencies.most_common())
